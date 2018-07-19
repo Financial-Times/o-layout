57e9356999f92e7066fe0c9f036dc70b40f3e26c
Gabrielle von Koss <gabivk@gmail.com>
2018-07-19 17:32:38 +0100

    break sass/js out into respective files for auto list/highlighting

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -1,24 +1,10 @@
 /*global require*/
+import oLayout from './../../main.js'
 
 //mega hack because we currently have no way of requesting brands for origami.json dependencies
 document.querySelector('link').href += "&brand=internal";
 
 document.addEventListener('DOMContentLoaded', () => {
 	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
+	oLayout.init(null, { constructNav: true })
 });
-
-//generate list automatically, should be oprtional
-let listItems = Array.from(document.querySelectorAll('h2, h3'), (header) => {
-	return `<li><a href='#${header.id}'>${header.innerText}</a></li>`;
-});
-
-let list = window.l = document.createElement('ol')
-list.classList.add('o-layout__navigation');
-list.innerHTML = listItems.join('');
-	
-document.querySelector('.o-layout__sidebar').append(list);
-
-//on page load, check url for permalink, if no permalink, highlight first list element
-location.hash ? list.querySelector(`a[href="${location.hash}"]`).classList.add('higlighted') : list.firstElementChild.classList.add('highlighted');
-
-// document.addEventListener('scroll')
---
+++ src/js/layout.js
@@ -0,0 +1,90 @@
+class Layout {
+
+	constructor (layoutEl, options) {
+		this.layoutEl = layoutEl;
+		this.headings = document.querySelectorAll('h2, h3');
+
+		this.options = Object.assign({}, {
+			baseClass: 'o-layout',
+			constructNav: false
+		}, options);
+
+		if (this.options.constructNav) {
+			this.constructNavFromDOM();
+		} else {
+			this.highlightNavItems(document.querySelector(`.${this.options.baseClass}__navigation`));
+		}
+
+	}
+
+	constructNavFromDOM () {
+		let listItems = Array.from(this.headings, (heading) => {
+			return `<li><a href='#${heading.id}'>${heading.innerText}</a></li>`;
+		});
+
+		list = document.createElement('ol')
+		list.classList.add(`${this.options.baseClass}__navigation`);
+		list.innerHTML = listItems.join('');
+
+		document.querySelector(`.${this.options.baseClass}__sidebar`).append(list);
+
+		this.highlightNavItems(list);
+	}
+
+
+	highlightNavItems(navigation) {
+		let currentLocation;
+		let navAnchors = navigation.querySelectorAll('A');
+
+		navAnchors.forEach((anchor, index) => {
+			if (location.hash === '' && index === 0) {
+				anchor.setAttribute('aria-current', 'location');
+			} else if (anchor.hash === location.hash) {
+				anchor.setAttribute('aria-current', 'location');
+			}
+		});
+
+		//on scroll, update current location in nav
+		document.addEventListener('scroll', () => {
+			let bufferedPageTop = window.pageYOffset + window.innerHeight / 6;
+			let possibleLocation;
+
+			this.headings.forEach(heading => {
+				if (heading.offsetTop <= bufferedPageTop) {
+					possibleLocation = `#${heading.id}`;
+				} else {
+					return false;
+				}
+
+				if (possibleLocation && possibleLocation !== currentLocation) {
+					navAnchors.forEach(anchor => {
+						if (anchor.hash === possibleLocation) {
+							anchor.setAttribute('aria-current', 'location');
+						} else {
+							anchor.setAttribute('aria-current', false);
+						}
+
+						currentLocation = possibleLocation;
+					});
+				} else if (!possibleLocation) {
+					navAnchors.forEach(anchor => anchor.setAttribute('aria-current', false));
+				}
+			})
+		});
+	}
+
+	static init (rootEl, opts) {
+		if (!rootEl) {
+			rootEl = document.body;
+		}
+		if (!(rootEl instanceof HTMLElement)) {
+			rootEl = document.querySelector(rootEl);
+		}
+		if (rootEl instanceof HTMLElement && rootEl.matches('[data-o-component=o-layout]')) {
+			return new Layout(rootEl, opts);
+		}
+		return Array.from(rootEl.querySelectorAll('[data-o-component="o-layout"]'), rootEl => new Layout(rootEl, opts));
+	}
+}
+
+export default Layout;
